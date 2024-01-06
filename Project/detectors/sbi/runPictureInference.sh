af2900a654a188cd13b9e6e867d4d35eab9b1558
Timotej979 <timotej.pet@gmail.com>
2024-01-06 17:46:20 +0100

    Add SBI detector

---
+++ Project/detectors/sbi/runPictureInference.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the CUDA_VISIBLE_DEVICES variable
+export CUDA_VISIBLE_DEVICES=*
+
+# Specify the path to the directory containing images
+image_directory="/datasets/face-forensics"
+
+# Specify the path to the weights file
+weights_file="weights/FFraw.tar"
+
+# Find all image files in the specified directory and its subdirectories
+find "$image_directory" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -print0 |
+while IFS= read -r -d '' image_file; do
+    # Execute the inference code for each image
+    python3 src/inference/inference_image.py -w "$weights_file" -i "$image_file"
+done
