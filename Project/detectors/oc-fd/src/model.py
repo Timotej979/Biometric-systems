6b15651a9355888635b29e6d11261d86e743421e
Timotej979 <timotej.pet@gmail.com>
2024-01-08 19:23:12 +0100

    Fix bugs in model.py, testing

--- Project/detectors/oc-fd/src/model.py
+++ Project/detectors/oc-fd/src/model.py
@@ -1,5 +1,5 @@
 # Import the standard libraries
-import os, re, csv, time, datetime, agparse
+import os, re, csv, time, datetime, argparse
 # Import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -30,51 +30,6 @@ from torch.utils.data import DataLoader
 from torchsummary import summary
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-
-####################################################################################################
-batch_size = 128
-epochs = 1
-no_cuda = False
-seed = 1
-log_interval = 50
-
-cuda = not no_cuda and torch.cuda.is_available()
-
-torch.manual_seed(seed)
-device = torch.device("cuda" if cuda else "cpu")
-print("Device: ", device)
-
-kwargs = {'num_workers': 8, 'pin_memory': True} if cuda else {}
-
-####################################################################################################
-# DATASET LOADING
-# Set your data root directories for train, validation, and test
-
-
-# Create image transformations
-transform_train = transforms.Compose([
-    transforms.Resize(100),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-transform_val_test = transforms.Compose([
-    transforms.Resize(100),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-# Create datasets using ImageFolder
-train_dataset = datasets.ImageFolder(train_root, transform=transform_train)
-val_dataset = datasets.ImageFolder(val_root, transform=transform_val_test)
-test_dataset = datasets.ImageFolder(test_root, transform=transform_val_test)
-
-# Create data loaders
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
 ####################################################################################################
@@ -194,9 +149,9 @@ class inferenceLoss(nn.Module):
         return loss_MSE
 
 
-##############################################################################################################
-############################################### MODELCONTROLER ###############################################
-##############################################################################################################
+###############################################################################################################
+############################################### MODEL CONTROLER ###############################################
+###############################################################################################################
 
 class ModelControler:
     def __init__(self, train_root, val_root, test_root, weights_path):
@@ -217,12 +172,13 @@ class ModelControler:
             self.test_root = test_root
 
         if weights_path is None:
-            self.weights_path = '/output/'
+            # Find most recent weights that are saved in the output directory
+            self.weights_path = max(glob.glob('/output/*/*.pth'), key=os.path.getctime)
 
         # Training parameters
         self.batch_size = 256
-        self.epochs = 200
-        self.logging_interval = 10
+        self.epochs = 100
+        self.logging_interval = 50
         self.lr_initial = 0.001
 
         # Output directory
@@ -266,19 +222,19 @@ class ModelControler:
         print("Creating the model...")
         model = VAE_CNN().to(device)
         optimizer = optim.Adam(model.parameters(), lr=self.lr_initial)
-        scheduler ReduceLROnPlateau(optimizer, mode='min', threshold_mode='rel', factor=0.1, patience=20, threshold=0.01, cooldown=0, eps=1e-5, verbose=True)
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', threshold_mode='rel', factor=0.1, patience=20, threshold=0.01, cooldown=0, eps=1e-5, verbose=True)
         loss_mse = customLoss()
 
         # For storing performance metrics in training
         train_losses = []
-        val_losses_bf = []
-        val_losses_m = []
+        val_losses_df = []
+        val_losses_r = []
         eers = []
         aucs = []
 
         # Print model and dataset stats
         print('\nModel summary:')
-        print(summary(self.model, (3, 100, 100)))
+        print(summary(model, (3, 100, 100)))
 
         print('\nDataset summary:')
         print("Train dataset:", train_dataset.classes)
@@ -292,9 +248,9 @@ class ModelControler:
         output_time_dir = f'output_train_{current_datetime}_{dataset_name}'
         os.makedirs(self.output_dir + output_time_dir, exist_ok=True)
 
-        log_fiename = f'log_{dataset_name}_{current_datetime}.txt'
+        log_filename = f'log_{dataset_name}_{current_datetime}.txt'
 
-        out_string = f"Training. Using {train_root} for training and {val_root} for validation."
+        out_string = f"Training. Using {self.train_root} for training and {self.val_root} for validation."
         print(out_string)
         with open(os.path.join(self.output_dir + output_time_dir, log_filename), 'a') as file:
             file.write(out_string)
@@ -326,7 +282,7 @@ class ModelControler:
                 optimizer.step()
 
                 # Logging
-                if batch_idx % logging_interval == 0:
+                if batch_idx % self.logging_interval == 0:
                     out_string = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                         epoch, batch_idx * len(data), len(train_loader.dataset),
                         batch_idx * len(data) * 100 / len(train_loader.dataset),
@@ -342,7 +298,7 @@ class ModelControler:
         
             out_string = '====> Epoch: {} Average loss: {:.4f}, learning rate: {}, time per epoch: {}'.format(epoch, train_loss / len(train_loader.dataset), optimizer.state_dict()['param_groups'][0]['lr'], epoch_elapsed_time)
             print(out_string)
-            with open(os.path.join(output_root_dir, log_filename), 'a') as file:
+            with open(os.path.join(self.output_dir + output_time_dir, log_filename), 'a') as file:
                 file.write(out_string)
                 file.write(os.linesep)
             train_losses.append(train_loss / len(train_loader.dataset))
@@ -353,8 +309,8 @@ class ModelControler:
 
             # List of losses for single validation run 
             val_losses_singlerun = []
-            val_losses_bf_singlerun = [] 
-            val_losses_m_singlerun = []
+            val_losses_df_singlerun = [] 
+            val_losses_r_singlerun = []
             val_labels_singlerun = []
 
 
@@ -369,17 +325,17 @@ class ModelControler:
 
                     # Separate scores by class. I refuse to do the arrayal gymnastics.
                     if label.item() == 0:
-                        val_losses_bf_singlerun.append(loss.item())
+                        val_losses_df_singlerun.append(loss.item())
                     else:
-                        val_losses_m_singlerun.append(loss.item())
+                        val_losses_r_singlerun.append(loss.item())
 
             # Validation loss
-            val_loss_bf = np.mean(val_losses_bf_singlerun)
-            val_loss_m = np.mean(val_losses_m_singlerun)
-            val_losses_bf.append(val_loss_bf)
-            val_losses_m.append(val_loss_m)
+            val_loss_df = np.mean(val_losses_df_singlerun)
+            val_loss_r = np.mean(val_losses_r_singlerun)
+            val_losses_df.append(val_loss_df)
+            val_losses_r.append(val_loss_r)
             # AUC
-            # Morphs tend to be more easily reproduced than bona-fide, which produces a lower loss for morphs, and flips the ROC curve.
+            # Deepfakes vs real images
             fpr, tpr, thresholds = metrics.roc_curve(val_labels_singlerun, val_losses_singlerun, pos_label=0) 
             roc_auc = metrics.auc(fpr, tpr)
             aucs.append(roc_auc)
@@ -392,21 +348,19 @@ class ModelControler:
             fnr_at_eer = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
 
             # Print validation results
-            out_string = f'====> Validation: bonafide loss: {val_loss_bf:.3f}, morph loss: {val_loss_m:.3f}, EER: {eer:.3f}, AUC: {roc_auc:.3f}, BPCER(FPR)@EER: {fpr_at_eer:.3f}, APCER(FNR)@EER: {fnr_at_eer:.3f}'
+            out_string = f'====> Validation: deepfake images loss: {val_loss_df:.3f}, real images loss: {val_loss_r:.3f}, EER: {eer:.3f}, AUC: {roc_auc:.3f}, BPCER(FPR)@EER: {fpr_at_eer:.3f}, APCER(FNR)@EER: {fnr_at_eer:.3f}'
             print(out_string)
-            with open(os.path.join(output_root_dir, log_filename), 'a') as file:
+            with open(os.path.join(self.output_dir + output_time_dir, log_filename), 'a') as file:
                 file.write(out_string)
                 file.write(os.linesep)
             
-            scheduler.step(val_loss_bf)
+            scheduler.step(val_loss_df)
             print(f'Number of bad epochs: {scheduler.num_bad_epochs}')
-
-            # TODO: Early stop
             # If scheduler reached the lr limit and there are too many bad epochs, early stop the training.
             if (scheduler.num_bad_epochs >= scheduler.patience) and (optimizer.state_dict()['param_groups'][0]['lr'] * scheduler.factor < scheduler.eps):
                 out_string = f'Early stopping.'
                 print(out_string)
-                with open(os.path.join(output_root_dir, log_filename), 'a') as file:
+                with open(os.path.join(self.output_dir + output_time_dir, log_filename), 'a') as file:
                     file.write(out_string)
                     file.write(os.linesep)
                 break
@@ -414,8 +368,8 @@ class ModelControler:
             # Save the weights every n-th epoch
             if (epoch % 10) == 0:
                 current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                filename = f'weights_autosave_{epoch}_train-{train_losses[-1]:.3f}_val-{val_losses_bf[-1]:.3f}_eer-{eers[-1]:.3f}_{current_datetime}.pth'
-                torch.save(model.state_dict(), os.path.join(output_root_dir, filename))
+                filename = f'weights_autosave_{epoch}_train-{train_losses[-1]:.3f}_val-{val_losses_df[-1]:.3f}_eer-{eers[-1]:.3f}_{current_datetime}.pth'
+                torch.save(model.state_dict(), os.path.join(self.output_dir + output_time_dir, filename))
 
 
         print('\n----------------------------------------------------------------')
@@ -426,19 +380,19 @@ class ModelControler:
 
         # Save the trained weights
         current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f'weights_final_train-{train_losses[-1]:.3f}_val-{val_losses_bf[-1]:.3f}_eer-{eers[-1]:.3f}_{current_datetime}.pth'
-        torch.save(model.state_dict(), os.path.join(output_root_dir, filename))
+        filename = f'weights_final_train-{train_losses[-1]:.3f}_val-{val_losses_df[-1]:.3f}_eer-{eers[-1]:.3f}_{current_datetime}.pth'
+        torch.save(model.state_dict(), os.path.join(self.output_dir + output_time_dir, filename))
 
         # Plot loss over time
         plt.figure(figsize=(15, 10))
         plt.plot(range(len(train_losses[1:])), train_losses[1:], c="dodgerblue")
-        plt.plot(range(len(val_losses_bf[1:])), val_losses_bf[1:], c="r")
+        plt.plot(range(len(val_losses_df[1:])), val_losses_df[1:], c="r")
         plt.title("Loss per epoch", fontsize=18)
         plt.xlabel("epoch", fontsize=18)
         plt.ylabel("loss", fontsize=18)
         plt.legend(['Training loss', 'Validation loss'], fontsize=18)
         filename = f'loss.svg'
-        plt.savefig( os.path.join(output_root_dir, filename))
+        plt.savefig( os.path.join(self.output_dir + output_time_dir, filename))
         
         # Plot equal error rate over time
         plt.figure(figsize=(15, 10))
@@ -449,19 +403,19 @@ class ModelControler:
         # plt.ylabel("EER, AUC", fontsize=18)
         plt.legend(['EER', 'AUC'], fontsize=18)
         filename = f'eer-auc.svg'
-        plt.savefig( os.path.join(output_root_dir, filename))
+        plt.savefig( os.path.join(self.output_dir + output_time_dir, filename))
 
-        # TODO: Plot bf eval vs morph eval loss over time. One should go down and the other up
-        # Plot bf vs morph loss over time 
+        # TODO: Plot df eval vs morph eval loss over time. One should go down and the other up
+        # Plot df vs morph loss over time 
         plt.figure(figsize=(15, 10))
-        plt.plot(range(len(val_losses_bf[1:])), val_losses_bf[1:], c="r")
-        plt.plot(range(len(val_losses_m[1:])), val_losses_m[1:], c="dodgerblue")
+        plt.plot(range(len(val_losses_df[1:])), val_losses_df[1:], c="r")
+        plt.plot(range(len(val_losses_r[1:])), val_losses_r[1:], c="dodgerblue")
         plt.title("Reconstruction score during validation", fontsize=18)
         plt.xlabel("epoch", fontsize=18)
         plt.ylabel("Reconstruction score", fontsize=18)
-        plt.legend(['Bonafide reconstruction score', 'Morph reconstruction score'], fontsize=18)
+        plt.legend(['Deepfake images reconstruction score', 'Real images reconstruction score'], fontsize=18)
         filename = f'reconstruction_score.svg'
-        plt.savefig( os.path.join(output_root_dir, filename))
+        plt.savefig( os.path.join(self.output_dir + output_time_dir, filename))
 
         plt.show()
         
@@ -469,7 +423,7 @@ class ModelControler:
 
 
     # Test model
-    def test_model(self)
+    def test_model(self):
         no_cuda = False
         cuda = not no_cuda and torch.cuda.is_available()
         
@@ -513,8 +467,8 @@ class ModelControler:
         scores = []
         labels = []
 
-        scores_bf = []
-        scores_m = []
+        scores_df = []
+        scores_r = []
 
         dataset_name = self.test_dataset.split(os.sep)[-1]
         current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -541,9 +495,9 @@ class ModelControler:
 
                 # Separate scores by class
                 if label.item() == 0:
-                    scores_bf.append(loss)
+                    scores_df.append(loss)
                 else:
-                    scores_m.append(loss)
+                    scores_r.append(loss)
 
         # Print total testing time
         end_time = time.time()
@@ -574,31 +528,31 @@ class ModelControler:
         fnr_at_eer = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
 
         # Store the labels and scores in a csv file
-        with open(os.path.join(output_root_dir, 'labels.csv'), 'w', newline='') as file:
+        with open(os.path.join(self.output_dir + output_time_dir, 'labels.csv'), 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(labels)
-        with open(os.path.join(output_root_dir, 'scores.csv'), 'w', newline='') as file:
+        with open(os.path.join(self.output_dir + output_time_dir, 'scores.csv'), 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(scores)
 
         # Log the output
         out_string = f'EER: {eer:.4f}, EER treshold: {eer_threshold:.4f}, BPCER(FPR)@EER: {fpr_at_eer:.4f}, APCER(FNR)@EER: {fnr_at_eer:.4f}'
         print(out_string)
-        with open(os.path.join(output_root_dir, log_filename), 'a') as file:
+        with open(os.path.join(self.output_dir + output_time_dir, log_filename), 'a') as file:
             file.write(out_string)
             file.write(os.linesep)
 
 
         # Plot the distribution 
         fig, axs = plt.subplots(2, 1, figsize=(10, 8))
-        min_score = min(min(scores_bf), min(scores_m))
-        max_score = max(max(scores_bf), max(scores_m))
-        axs[0].hist(scores_bf, bins=30, color='blue', alpha=0.7)
+        min_score = min(min(scores_df), min(scores_r))
+        max_score = max(max(scores_df), max(scores_r))
+        axs[0].hist(scores_df, bins=30, color='blue', alpha=0.7)
         axs[0].set_title('Reconstruction score distribution')
         axs[0].axvline(eer_threshold, color='red', linestyle='dashed', linewidth=2)  # Draw treshold
         axs[0].set_xlim(min_score, max_score)
         axs[0].set_ylabel('Frequency')
-        axs[1].hist(scores_m, bins=30, color='green', alpha=0.7)
+        axs[1].hist(scores_r, bins=30, color='green', alpha=0.7)
         axs[1].axvline(eer_threshold, color='red', linestyle='dashed', linewidth=2)  # Draw treshold
         axs[1].set_xlim(min_score, max_score)
         axs[1].set_xlabel('Reconstruction score')
--- Project/evaluation-scripts/evaluateDetectors.sh
+++ Project/evaluation-scripts/evaluateDetectors.sh
@@ -17,6 +17,7 @@ printenv | grep DETECTOR_OUTPUT_DIR
 # Run the docker images for every detector, mounting the volumes created above
 # Add the following flags to the docker run command to enable GPU support and increase the shared memory size (After the -it flag):
 #   --gpus all --shm-size 64G 
+#   -it --gpus all --shm-size 64G
 
-docker run --gpus all --shm-size 64G -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/oc-fd:/output oc-fd-image /bin/bash
+docker run -it -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/oc-fd:/output oc-fd-image /bin/bash
 #docker run --gpus all -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/sbi:/output sbi-image /bin/bash
