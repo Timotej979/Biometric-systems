4e600e710e6db67c824340dae08c4ec3fe7f2813
Timotej979 <timotej.pet@gmail.com>
2024-01-08 19:25:36 +0100

    Modify evaluateDetectors

--- Project/evaluation-scripts/evaluateDetectors.sh
+++ Project/evaluation-scripts/evaluateDetectors.sh
@@ -16,8 +16,8 @@ printenv | grep DETECTOR_OUTPUT_DIR
 
 # Run the docker images for every detector, mounting the volumes created above
 # Add the following flags to the docker run command to enable GPU support and increase the shared memory size (After the -it flag):
-#   --gpus all --shm-size 64G 
-#   -it --gpus all --shm-size 64G
+#   --gpus all --shm-size 16G 
+#   -it --gpus all --shm-size 16G
 
-docker run -it -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/oc-fd:/output oc-fd-image /bin/bash
+docker run --gpus all --shm-size 16G -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/oc-fd:/output oc-fd-image /bin/bash
 #docker run --gpus all -v $DATASET_DIR:/datasets:ro -v $DETECTOR_OUTPUT_DIR/sbi:/output sbi-image /bin/bash
