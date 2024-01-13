4e129974d460b59cc09b70b293447129ac1add0d
Timotej979 <timotej.pet@gmail.com>
2024-01-13 21:12:46 +0100

    Set worker default to 1 since mybi problem with cv

--- Project/detectors/sbi/run.py
+++ Project/detectors/sbi/run.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run inference on a test set.')
     parser.add_argument('--test_root', type=str, default=None, help='The root directory of the test set.')
     parser.add_argument('--weights_bool', type=bool, default=True, help='The weights to use for inference.')
-    parser.add_argument('--worker_num', type=int, default=4, help='The number of workers to use for inference.')
+    parser.add_argument('--worker_num', type=int, default=1, help='The number of workers to use for inference.')
     args = parser.parse_args()
 
     # Create the SBIControler object
